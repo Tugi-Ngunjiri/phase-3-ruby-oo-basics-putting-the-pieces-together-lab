c6f4b67c656e70011bbf221d977669b52af4ffd1
tugi <blueqwinn@gmail.com>
2023-02-09 19:46:57 +0300

    First commit

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,37 @@
+class Book
+ attr_reader : title
 
+ 
+    def initialize
+       @book = Book.new 
+    end
+ end
 
+ title = Book.new("And Then There Were None")
+
+title.book # => "And Then There Were None"
+
+class Book{
+constructor(book){
+this.book  = book;
+
+}
+}
+
+const book = new.title("And Then There Were None")
+book.title # => "And Then There Were None"
+
+def initialize
+    attr_accessor : name
+end
+ def initialize 
+    attr_accessor :book,
+    page.number = "10 page counts"
+    book.genre = "Genre"
+ end
+
+ def initialize 
+    attr_reader : page
+    page.turns = "Can Make 10 Turns"
+   end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
 # Make your shoe class here!
+class shoe
+    attr_accessor : brand
+end
+def initialize(brand)
+    brand = brand.new
+    brand.colour = "#FFFFFF" 
+    brand.size = 10
+    brand.material = "#FFFFFF"
+    brand.condition = true
+end
+
+def initialize(cobble)
+    cobble = cobble.new
+
+    cobble.new = "Shoe is Getting Repaired"
+    cobble.new 
+    cobble.condition = new 
+  end
+end
